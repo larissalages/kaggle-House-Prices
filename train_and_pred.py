91049434e3a49c2399faa02ff95576c80f9d11f7
larissalages <llo@cin.ufpe.br>
2019-09-19 19:34:22 -0400

    add new train methods

--- train_and_pred.py
+++ train_and_pred.py
@@ -3,6 +3,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
 
 
 def train_pred_LinearRegression(X_train, y_train, X_test, grid_search=False):
@@ -29,3 +32,36 @@ def train_pred_RandomFlorest(X_train, y_train, X_test, grid_search=False):
 
 	return reg, y_pred
 
+def train_pred_GradientBoostingRegressor(X_train, y_train, X_test, grid_search=False):
+	if grid_search==False:
+		reg = GradientBoostingRegressor(max_features='sqrt',loss='huber').fit(X_train, y_train)
+		y_pred = reg.predict(X_test)
+	else:
+		param_grid = {'loss' : ['ls', 'lad', 'huber', 'quantile'], 'learning_rate' : [0.01, 0.1, 1],'n_estimators' : [100,500, 1000]}
+		grid_rf = GridSearchCV(GradientBoostingRegressor(), param_grid, cv=10)
+		reg = grid_rf.fit(X_train, y_train)
+		y_pred = reg.predict(X_test)
+
+	return reg, y_pred
+
+def train_pred_MLPRegressor(X_train, y_train, X_test, grid_search=False):
+	if grid_search==False:
+		reg = MLPRegressor().fit(X_train, y_train)
+		y_pred = reg.predict(X_test)
+	else:
+		param_grid = {'hidden_layer_sizes' : [100,(100,50),(100,50,20)], 'solver' : ['lbfgs', 'sgd', 'adam'],  }
+		grid_rf = GridSearchCV(MLPRegressor(learning_rate = 'adaptive', activation = 'logistic'), param_grid, cv=10)
+		reg = grid_rf.fit(X_train, y_train)
+
+	return reg, y_pred
+
+def train_pred_SVR(X_train, y_train, X_test, grid_search=False):
+	if grid_search==False:
+		reg = SVR().fit(X_train, y_train)
+		y_pred = reg.predict(X_test)
+	else:
+		param_grid = {'kernel' : ['rbf','sigmoid'], 'C' : [0.01,0.1,1,10,100,1000], 'gamma': [0.01,0.1,1,10,100]  }
+		grid_rf = GridSearchCV(SVR(), param_grid, cv=10)
+		reg = grid_rf.fit(X_train, y_train)
+
+	return reg, y_pred
